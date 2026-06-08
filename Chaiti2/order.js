a196543eabd77307ba52e8c2ae1e25be639ade4a
polina5566hpl-beep <polina.55.66hpl@gmail.com>
2026-06-08 15:59:58 +0100

    Update Google Script URL in sendToGoogleScript function

--- Chaiti2/order.js
+++ Chaiti2/order.js
@@ -355,7 +355,7 @@ function showThankYouModal(orderId) {
 // ===== إرسال الطلب إلى Google Script (بدون حفظ محلي) =====
 async function sendToGoogleScript(orderData) {
     try {
-        const scriptUrl = 'https://script.google.com/macros/s/AKfycbyHUuqC7llgNrkPm69HEEG1UMVHyyM9wapbK-ONwHEquOdwHJhx9H6FQbpHl-E7MHqY/exec';
+        const scriptUrl = 'https://script.google.com/macros/s/AKfycbwgsE-r3ufzkCCoJPck1q_4_6g-aqPSqu6ofg2hJ6s/dev';
         
         // إظهار رسالة "جاري الإرسال..."
         showOrderAlert('📡 جاري إرسال الطلب إلى الخادم...', 'info');
